8cd34dfd7a8857a605987880b4077f95f2994ab5
Vladimir Nekrasov <todrinktea5@gmail.com>
2019-09-05 17:22:39 +0300

    added weird onSettingsChanged thing

--- dist/main.js
+++ dist/main.js
@@ -3,11 +3,12 @@ console.log('main.js pass');
 VK.init(function() { 
     // API initialization succeeded 
     // Your code here 
-    VK.callMethod("showSettingsBox", 0);
+    VK.callMethod("showSettingsBox", 0).onSettingsChanged(
     VK.api("fave.get",{"extended": "0", "item_type": "post", "tag_id": "", "offset": "0", "count": "50", "fields": "", "is_from_snackbar": "0", "v": "5.101"}, function(data) {
         console.log('callback worked');
         console.log(data);
     })
+    );
  }, function() { 
     // API initialization failed 
     // Can reload page here 
