107fb8467fe73e91d336c44ea089ad1d773848e3
Vladimir Nekrasov <todrinktea5@gmail.com>
2019-09-05 17:43:27 +0300

    cleaned some errors, added console logs

--- dist/main.js
+++ dist/main.js
@@ -6,13 +6,12 @@ VK.init(function() {
     // Your code here 
     VK.callMethod("showSettingsBox", 0);
     console.log('access check worked');
-    btn.addEventListener(onSettingsChanged,
-        console.log('onSettingsChanged check pass');
-        VK.api("fave.get",{"extended": "0", "item_type": "post", "tag_id": "", "offset": "0", "count": "50", "fields": "", "is_from_snackbar": "0", "v": "5.101"}, function(data) {
-            console.log('callback worked');
-            console.log(data);
-        })
-    );
+    btn.addEventListener("click",function() {
+    console.log("event listener worked");
+    VK.api("fave.get",{"extended": "0", "item_type": "post", "tag_id": "", "offset": "0", "count": "50", "fields": "", "is_from_snackbar": "0", "v": "5.101"}), function(data) {
+        console.log('callback worked');
+        console.log(data);
+    }});
  }, function() { 
     // API initialization failed 
     // Can reload page here 
