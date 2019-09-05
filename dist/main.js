b2677cc77ffaaeb506ca9cda88b95488580c4370
Vladimir Nekrasov <todrinktea5@gmail.com>
2019-09-05 16:31:12 +0300

    added console logs for testing i

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,10 @@
+console.log('main.js pass');
+
 VK.init(function() { 
     // API initialization succeeded 
     // Your code here 
     VK.api("fave.get",{"extended": "0", "item_type": "post", "tag_id": "0", "offset": "0", "count": "50", "fields": "first-name", "is_from_snackbar": "0", "v": "5.101"}, function(data) {
+        console.log('callback worked');
         console.log(data);
     })
  }, function() { 
