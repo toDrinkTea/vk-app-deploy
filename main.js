20daed20a32afad03832c5ad1b33c2d84242fc37
Vladimir Nekrasov <todrinktea5@gmail.com>
2019-09-05 18:31:15 +0300

    Updates

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ VK.init(function() {
     VK.callMethod("showSettingsBox", 0);
     console.log('access check worked');
     
-    VK.addCallback('onSettingsChanged', function(){
+    VK.addCallback('onSettingsChanged', function(e){
+        console.log(e);
         console.log('this worked');
         VK.api("fave.get",{"extended": "0", "item_type": "post", "tag_id": "", "offset": "0", "count": "50", "fields": "", "is_from_snackbar": "0", "v": "5.101"}), function(data){
             console.log('callback worked');
