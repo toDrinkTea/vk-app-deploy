a3fa1b71b2bed5ade3dd9c30f575c49ef4511128
Vladimir Nekrasov <todrinktea5@gmail.com>
2019-09-05 16:49:39 +0300

    changed request a little bit

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,7 @@ console.log('main.js pass');
 VK.init(function() { 
     // API initialization succeeded 
     // Your code here 
-    VK.api("fave.get",{"extended": "0", "item_type": "post", "tag_id": "0", "offset": "0", "count": "50", "fields": "first-name", "is_from_snackbar": "0", "v": "5.101"}, function(data) {
+    VK.api("fave.get",{"extended": "0", "item_type": "post", "tag_id": "", "offset": "0", "count": "50", "fields": "", "is_from_snackbar": "0", "v": "5.101"}, function(data) {
         console.log('callback worked');
         console.log(data);
     })
