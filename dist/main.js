42938342b9b562b4dc950f0e1739c28505ca75e5
Vladimir Nekrasov <todrinktea5@gmail.com>
2019-09-05 16:54:13 +0300

    added access request to everything

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,7 @@ console.log('main.js pass');
 VK.init(function() { 
     // API initialization succeeded 
     // Your code here 
+    VK.callMethod("showSettingsBox", 0);
     VK.api("fave.get",{"extended": "0", "item_type": "post", "tag_id": "", "offset": "0", "count": "50", "fields": "", "is_from_snackbar": "0", "v": "5.101"}, function(data) {
         console.log('callback worked');
         console.log(data);
