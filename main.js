e54c44ce6fbe9130679d7d019e86110be72d68ce
Vladimir Nekrasov <todrinktea5@gmail.com>
2019-09-05 17:36:40 +0300

    Updates

--- main.js
+++ main.js
@@ -5,15 +5,17 @@ VK.init(function() {
     // API initialization succeeded 
     // Your code here 
     VK.callMethod("showSettingsBox", 0);
-    
+    console.log('access check worked');
+    btn.addEventListener(onSettingsChanged,
+        console.log('onSettingsChanged check pass');
+        VK.api("fave.get",{"extended": "0", "item_type": "post", "tag_id": "", "offset": "0", "count": "50", "fields": "", "is_from_snackbar": "0", "v": "5.101"}, function(data) {
+            console.log('callback worked');
+            console.log(data);
+        })
+    );
  }, function() { 
     // API initialization failed 
     // Can reload page here 
 }, '5.101'); 
 
-btn.addEventListener(onSettingsChanged,
-    VK.api("fave.get",{"extended": "0", "item_type": "post", "tag_id": "", "offset": "0", "count": "50", "fields": "", "is_from_snackbar": "0", "v": "5.101"}, function(data) {
-        console.log('callback worked');
-        console.log(data);
-    })
-);
+
