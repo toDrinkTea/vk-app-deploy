be5d7fce68f5da90faf6abf5f5d1d51855205d5d
Vladimir Nekrasov <todrinktea5@gmail.com>
2019-09-05 18:12:12 +0300

    one more time, same thing

--- dist/main.js
+++ dist/main.js
@@ -9,16 +9,16 @@ VK.init(function() {
     
     VK.addCallback('onSettingsChanged', function(){
         console.log('this worked');
-
+        VK.api("fave.get",{"extended": "0", "item_type": "post", "tag_id": "", "offset": "0", "count": "50", "fields": "", "is_from_snackbar": "0", "v": "5.101"}), function(data){
+            console.log('callback worked');
+            console.log(data);
+        }
     });
     
     // This didn't work
     // btn.addEventListener("click",function() {
     // console.log("event listener worked");
-    // VK.api("fave.get",{"extended": "0", "item_type": "post", "tag_id": "", "offset": "0", "count": "50", "fields": "", "is_from_snackbar": "0", "v": "5.101"}), function(data) {
-    //     console.log('callback worked');
-    //     console.log(data);
-    // }});
+    // });
 
 
 
