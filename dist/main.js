2226daa2949993ad3100703785f3b4315cd32a1d
Vladimir Nekrasov <todrinktea5@gmail.com>
2019-09-05 17:23:30 +0300

    added console log to weird thing

--- dist/main.js
+++ dist/main.js
@@ -4,6 +4,7 @@ VK.init(function() {
     // API initialization succeeded 
     // Your code here 
     VK.callMethod("showSettingsBox", 0).onSettingsChanged(
+    console.log('weird thing worked');
     VK.api("fave.get",{"extended": "0", "item_type": "post", "tag_id": "", "offset": "0", "count": "50", "fields": "", "is_from_snackbar": "0", "v": "5.101"}, function(data) {
         console.log('callback worked');
         console.log(data);
