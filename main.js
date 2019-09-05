348c73ae2e73b64db8569ff2c53c15c0e96ee998
Vladimir Nekrasov <todrinktea5@gmail.com>
2019-09-05 15:39:35 +0300

    Updates

---
+++ main.js
@@ -0,0 +1,8 @@
+VK.init(function() { 
+    // API initialization succeeded 
+    // Your code here 
+    VK.callMethod("showSettingsBox", 8214);
+ }, function() { 
+    // API initialization failed 
+    // Can reload page here 
+}, '5.101'); 
